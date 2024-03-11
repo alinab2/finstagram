da95a196360ad8a420769623c9fb3dca7076c436
alinab2 b <alinasbhatia@gmail.com>
2024-03-11 23:51:42 +0000

    W4D1

--- demo.rb
+++ demo.rb
@@ -1,68 +1,34 @@
+class Human
+    def walk
+    puts "I can walk"
+    end
 
-def check_age(age, name)
-    if age > 80
-        puts "#{name}, you are able to buy that drink"
-    elsif age > 18
-        puts "#{name}, are you sure you want to buy that"
-    else
-        puts "#{name} you are not able to buy that drink"
+    def talk
+        puts "i can talk"
     end
 end
 
+alina = Human.new
 
-names = ["alina", "ali", "alin", "alize"]
+alina.walk
+alina.talk
 
-
-names.each do |name|
-    puts name + " is cool!"
-end
-
-people = [
-    {
-    name: "alina",
-    age: 30
-    }, 
-    {
-    name: "ali",
-    age:45
-    },
-    {
-    name: "alin",
-     age:35
-    },
-     {
-    name: "alize",
-    age:23
-    }
-]
-for p in people 
-   if p[:age] > 31 
-        puts p[:name] + ", you get a discount"
-   else 
-        puts p[:name] + ", you get a 10% discount"
-   end
-end
-
-for n in (1..100)
-    if n % 3==0
-        puts "fizz"
-    else
-        puts n
-    end
-
-    if n % 5 == 0
-        puts "buzz"
-    else
-        puts n
-    end
-    if n % 3 == 0 and n % 5 == 0
-        puts "fizzbuzz"
-    else 
-        puts n
+class Superhuman < Human
+    def fly
+        puts "I can fly"
     end
 end
 
-<% for i in (1..10000) %>
-    <%= i %>
-<% end %>
+class UltraHuman < Superhuman
+end
+
+ali = Superhuman.new
+liz = UltraHuman.new
+
+liz.walk
+liz.talk
+liz.fly
+
+
+
 
